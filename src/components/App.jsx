10472fb7fef72387e2f0d721ce933462f50806cc
Iryna Samsonenko <samsonenko.iryna@gmail.com>
2024-03-09 15:03:11 +0200

    save3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 const SharedLayout = lazy(() => import('./SharedLayout/SharedLayout'));
 const Home = lazy(() => import('../pages/Home/Home'));
@@ -12,19 +12,17 @@ const NotFoundPage = lazy(() => import('./NotFoundPage/NotFoundPage'));
 const App = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<SharedLayout />}>
-            <Route index element={<Home />} />
-            <Route path="movies" element={<Movies />} />
-            <Route path="movies/:movieId" element={<MovieDetails />}>
-              <Route path="cast" element={<Cast />} />
-              <Route path="reviews" element={<Reviews />} />
-            </Route>
-            <Route path="*" element={<NotFoundPage />} />
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
-        </Routes>
-      </Router>
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
     </Suspense>
   );
 };
